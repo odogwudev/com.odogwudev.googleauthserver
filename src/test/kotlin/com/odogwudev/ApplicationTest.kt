ba47c7dc07d7f1fa60a496669bee00f2b8aed22b
Michael Enoma <odogwudev@e.email>
2022-05-16 18:14:36 +0100

    Application Test

--- src/test/kotlin/com/odogwudev/ApplicationTest.kt
+++ src/test/kotlin/com/odogwudev/ApplicationTest.kt
@@ -1,31 +1,16 @@
 package com.odogwudev
 
-import io.ktor.server.routing.*
+import com.odogwudev.plugins.*
+import io.ktor.client.plugins.cookies.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
 import io.ktor.http.*
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.plugins.*
-import org.slf4j.event.*
-import io.ktor.server.request.*
-import io.ktor.server.sessions.*
-import io.ktor.server.auth.*
-import io.ktor.util.*
-import io.ktor.server.application.*
-import io.ktor.server.response.*
-import io.ktor.client.request.*
-import io.ktor.client.statement.*
-import kotlin.test.*
 import io.ktor.server.testing.*
-import com.odogwudev.plugins.*
+import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class ApplicationTest {
-    @Test
-    fun testRoot() = testApplication {
-        application {
-            configureRouting()
-        }
-        client.get("/").apply {
-            assertEquals(HttpStatusCode.OK, status)
-            assertEquals("Hello World!", bodyAsText())
-        }
-    }
+
 }
